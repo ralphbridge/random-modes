c28d191e54efdb909962aa0527e1e12738a44904
ralphqm <raulpuente@ciencias.unam.mx>
2019-06-26 21:25:51 -0500

    Ready for efficiency test

--- random_shell.m
+++ random_shell.m
@@ -1,7 +1,9 @@
 clear all
 clc
 
-Nmod=100000; %Number of modes
+%%%%%%%%%%%%%%% Inverse transform method %%%%%%%%%%%%%%%%%%%
+
+Nmod=10000; %Number of modes
 Rin=11; % Internal radius
 Rout=19; % External radius
 
@@ -9,10 +11,9 @@ u=(Rout^3-Rin^3)*rand(Nmod,1)+Rin^3;
 v=rand(Nmod,1);
 w=rand(Nmod,1);
 
-%%%%%%%%%%%%%%% Inverse transform method %%%%%%%%%%%%%%%%%%%
-
 theta=zeros(Nmod,1);
 phi=zeros(Nmod,1);
+r=u;
 
 for i=1:Nmod % This generates random numbers uniformly over the surface of a sphere of radius 1
     theta(i)=acos(1-2*v(i));
@@ -28,12 +29,13 @@ axis equal
 xlabel('$x$','interpreter','latex','fontsize',20)
 ylabel('$y$','interpreter','latex','fontsize',20)
 zlabel('$z$','interpreter','latex','fontsize',20)
-title('Sampling numbers over the surface of a sphere of radius $1$','interpreter','latex','fontsize',20)
+title('Sampling numbers over the surface of a sphere of radius $1$ using the inverse transform method','interpreter','latex','fontsize',10)
 
 for i=1:Nmod % This rescales the numbers with the uniformly generated random number u inside the shell
     x(i)=x(i)*(u(i))^(1/3);
     y(i)=y(i)*(u(i))^(1/3);
     z(i)=z(i)*(u(i))^(1/3);
+    r(i)=sqrt(x(i)^2+y(i)^2+z(i)^2);
 end
 
 figure
@@ -43,7 +45,7 @@ axis equal
 xlabel('$x$','interpreter','latex','fontsize',20)
 ylabel('$y$','interpreter','latex','fontsize',20)
 zlabel('$z$','interpreter','latex','fontsize',20)
-title('Sampling numbers inside the spherical shell of radii $R_{out}$ and $R_{in}$','interpreter','latex','fontsize',20)
+title('Sampling numbers inside the spherical shell of radii $R_{out}$ and $R_{in}$ using the inverse transform method','interpreter','latex','fontsize',10)
 
 j=1;
 for i=1:Nmod
@@ -60,4 +62,67 @@ grid on
 axis equal
 xlabel('$y$','interpreter','latex','fontsize',20)
 ylabel('$z$','interpreter','latex','fontsize',20)
-title('Cross section of the shell of radii $R_{out}$ and $R_{in}$','interpreter','latex','fontsize',20)
+title('Cross section of the shell of radii $R_{out}$ and $R_{in}$ using the inverse transform method','interpreter','latex','fontsize',10)
+
+clear all
+
+%%%%%%%%%%%%%%% Gaussian method %%%%%%%%%%%%%%%%%%%
+
+Nmod=10000; %Number of modes
+Rin=11; % Internal radius
+Rout=19; % External radius
+
+t=(Rout^3-Rin^3)*rand(Nmod,1)+Rin^3;
+u=randn(Nmod,1);
+v=randn(Nmod,1);
+w=randn(Nmod,1);
+
+for i=1:Nmod
+    x(i)=u(i)/sqrt(u(i)^2+v(i)^2+w(i)^2);
+    y(i)=v(i)/sqrt(u(i)^2+v(i)^2+w(i)^2);
+    z(i)=w(i)/sqrt(u(i)^2+v(i)^2+w(i)^2);
+end
+
+figure
+plot3(x,y,z,'.')
+grid on
+axis equal
+xlabel('$x$','interpreter','latex','fontsize',20)
+ylabel('$y$','interpreter','latex','fontsize',20)
+zlabel('$z$','interpreter','latex','fontsize',20)
+title('Sampling numbers over the surface of a sphere of radius $1$ using the gaussian method','interpreter','latex','fontsize',10)
+
+for i=1:Nmod % This rescales the numbers with the uniformly generated random number u inside the shell
+    x(i)=x(i)*(t(i))^(1/3);
+    y(i)=y(i)*(t(i))^(1/3);
+    z(i)=z(i)*(t(i))^(1/3);
+    r(i)=sqrt(x(i)^2+y(i)^2+z(i)^2);
+    theta(i)=acos(z(i)/r(i));
+    phi(i)=atan(y(i)/x(i));
+end
+
+figure
+plot3(x,y,z,'.')
+grid on
+axis equal
+xlabel('$x$','interpreter','latex','fontsize',20)
+ylabel('$y$','interpreter','latex','fontsize',20)
+zlabel('$z$','interpreter','latex','fontsize',20)
+title('Sampling numbers inside the spherical shell of radii $R_{out}$ and $R_{in}$ using the inverse transform method','interpreter','latex','fontsize',10)
+
+j=1;
+for i=1:Nmod
+    if(abs(x(i))<Rout/10)
+        yproy(j)=y(i);
+        zproy(j)=z(i);
+        j=j+1;
+    end
+end
+
+figure
+plot(yproy,zproy,'.')
+grid on
+axis equal
+xlabel('$y$','interpreter','latex','fontsize',20)
+ylabel('$z$','interpreter','latex','fontsize',20)
+title('Cross section of the shell of radii $R_{out}$ and $R_{in}$ using the gaussian method','interpreter','latex','fontsize',10)
